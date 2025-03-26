c7721b4d1f6d2d81b9406c7d6d55e13fa6b43c89
Rubik <25591967+rubiksw@users.noreply.github.com>
2025-03-25 23:03:20 -0400

    Create cl_config.lua

---
+++ cl_config.lua
@@ -0,0 +1,192 @@
+Guide = Guide || {}
+Guide.Config = Guide.Config || {}
+
+Guide.Config.Pages = { -- That's the structure that I'm aiming for
+    [1] = {
+        strName = "Introduction",
+        tblSlides = {
+            [1] = {
+                strDescription = "Welcome to our server! TitsRP is a custom Roleplay gamemode based on a heavily modified version of DarkRP. On TitsRP you are able to roleplay as over 100 different jobs, each with their own unique functionality. To learn more about each job and their purpose, click the 'Jobs' tab.",
+                strImagePath = "ENc8hV6"
+            },
+            [2] = {
+                strDescription = "Earn XP to unlock over 75 different perks and talents. Some of the most common ways of earning XP include playing as roleplay jobs, events, fishing, farming, mining, and cooking, but there are too many ways to list here!",
+                strImagePath = "fE50AQM"
+            },
+            [3] = {
+                strDescription = "You can join gangs with other players or create your own to unlock special features and abilities. Level up your gang by earning XP and doing quests. Each gang member’s XP gained will count towards leveling up so it’s important to fill your gang with players that are active and enjoy the grind.",
+                strImagePath = "mKYXLxQ"
+            },
+            [4] = {
+                strDescription = "Sugar Dollars (or 'Suga') is a premium in-game currency. Sugar Dollars can be used to purchase almost anything in the server. Sugar Dollars have a much higher value than DarkRP cash, so it is perfect for large player-to-player trades, Custom Classes and weapon & playermodel perms. Suga can not be taxed by the government! You can view your current Sugar Dollar balance on the HUD at the bottom left of your screen.",
+                strImagePath = "RoL4g9Q"
+            },
+            [5] = {
+                strDescription = "The MOTD is the central hub for useful server information such as changelogs, stats, daily rewards, double XP jobs & your personal mailbox. Open the MOTD by typing !MOTD in chat. Your mailbox is accessible from the top right of the menu. Check this often as the server may send you important information related to Auction House transactions, leaderboards, etc.",
+                strImagePath = "kxAwVix"
+            },
+        }
+    },
+    [2] = {
+        strName = "XP & Leveling",
+        tblSlides = {
+            [1] = {
+                strDescription = "The Player Leveling System was introduced to the server to provide players who are not in a gang an opportunity to progress their character and unlock special perks and talents. This guide will cover the basics of the Player Leveling System: How to level up and the benefits of leveling up.",
+                strImagePath = "fE50AQM"
+            },
+            [2] = {
+                strDescription = "Players can level up by gaining XP. Before, players could only earn XP if they were in a gang. Now with the introduction of the new Leveling System, basically anything you do in the server will earn you XP, whether or not you are in a gang!",
+                strImagePath = "jUhsMgp"
+            },
+            [3] = {
+                strDescription = "You will earn one Perk per level-up. You can view your currently active Perks by clicking the Leveling tab in the F4 menu. These Perks are designed to enhance Roleplay for experienced players looking for even greater rewards for their labors.",
+                strImagePath = "cl1rwGK"
+            },
+            [4] = {
+                strDescription = "Talents are a bit different from Perks as they must be chosen by the player and cost Talent Points. Players will earn Talent Points simply by leveling up. In the Talents tab of the leveling menu you will see a list of available Talents and their cost in Talent Points.",
+                strImagePath = "JA8Vudh"
+            },
+            [5] = {
+                strDescription = "Your total XP earned for the day is added to your 'Pooled XP'. You can cash out your Pooled XP at any time until the server resets for either Tier 1 or Tier 2 Lootboxes. To cash out your Pooled XP, visit the lootbox seller NPC near fountain.",
+                strImagePath = "yL9nIt5"
+            },
+        }
+    },
+    [3] = {
+        strName = "Gangs",
+        tblSlides = {
+            [1] = {
+                strDescription = "You can join gangs with other players or create your own to unlock special features and abilities. This guide will cover the basics of how to create your own gang and some of the perks of doing so. Level up your gang by earning XP and doing quests. Each gang member’s XP gained will count towards leveling up so it’s important to fill your gang with players that are active and enjoy the grind.",
+                strImagePath = "5uNtsLm"
+            },
+            [2] = {
+                strDescription = "Gain XP faster by joining a gang and working as a team. Each time you level up your gang will unlock a new Gang Perk. Every 10 levels your gang will unlock a new Gang Talent.",
+                strImagePath = "pqkovCi"
+            },
+            [3] = {
+                strDescription = "You can join an existing gang to unlock perks they've already earned or create your own and start fresh. Visit the Gang Creator NPC to create your gang. There are no requirements to creating a gang, as long as you have sufficient funds. Access your gang by typing !GangMenu in chat. There you can invite new members & promote existing members to a higher rank.",
+                strImagePath = "bFy36uQ"
+            },
+            [4] = {
+                strDescription = "Similar to the player leveling system, gangs earn Perks each time they level up. A new gang Talent is unlocked every 10 levels and are organized by categories - All Purpose, Thief Spec, Police Spec, and Misc Spec. Depending on what route you want to take with your gang - whether your a casual PVE gang or hardcore Civil Protection sweat, there are unique Talents tailored for you.",
+                strImagePath = "rTY5LLK"
+            },
+            [5] = {
+                strDescription = "The leader of a gang can initiate Gang Wars with other gangs. The gang with the most kills against the enemy gang with be declared the winner. If you manage to kill the Gang owner, you automatically win the Gang War. Open the Gang Menu by typing !gangMenu in chat. At the top left you will see the option to Start War.",
+                strImagePath = "yRzBbjV"
+            },
+        }
+    },
+    [4] = {
+        strName = "Leaderboards",
+        tblSlides = {
+            [1] = {
+                strDescription = "As you complete tasks to earn XP such as quests, treasure hunting, farming, etc. you are entered into the Leaderboards. At the end of the week, the person with the highest score is rewarded with Tier 3 Lootbox tokens. Tier 3 Lootboxes contain extremely valuable loot.",
+                strImagePath = "kPRVZBK"
+            },
+            [2] = {
+                strDescription = "To view the Leaderboards and your progress, open the F4 menu and click Leaderboards. At the top of the page you will see the time at which the Leaderboards will reset as well as the current reward. Your progress & the leader's progress will be displayed individually under each Leaderboard.",
+                strImagePath = "HU3jS1D"
+            },
+        }
+    },
+    [5] = {
+        strName = "Talismans",
+        tblSlides = {
+            [1] = {
+                strDescription = "Talismans can be crafted, found in lootboxes, or purchased on the Auction House and provide your character with any number of temporary buffs. ",
+                strImagePath = "Nt16gM0"
+            },
+            [2] = {
+                strDescription = "To craft a talisman, become a Spirit Caller. You need Spirit Jars in order to craft Talismans. Hit players who have pooled XP with your Spirit Caller's Knife. Players who have pooled over 150XP have a spirit to collect (75XP with gang perk). You can also extract spirit from yourself.",
+                strImagePath = "ACI8Hs6"
+            },
+            [3] = {
+                strDescription = "You will also need to collect Dark Spirit Jars to craft a Talisman. Dark Spirit Jars are obtained from players who have spirit to collect as well as a bounty worth over $3000.",
+                strImagePath = "dBWZAmE"
+            },
+        }
+    },
+    [6] = {
+        strName = "Custom Classes",
+        tblSlides = {
+            [1] = {
+                strDescription = "A custom class is a player-owned job based off either the Thief class or a Civil Protection class. Players can customize the weapon loadout and playermodel. It has a custom name, color and two friend slots that players can customize. CCs won’t have special RP permissions.",
+                strImagePath = "owaAO7x"
+            },
+            [2] = {
+                strDescription = "You can earn or buy enough cash to create a custom class using the CC Creator NPC at fountain. You can add, remove and swap weapons “slots”. Each slot can be purchased from the CC editor using DarkRP cash or Sugar Dollars.",
+                strImagePath = "3b8Or2T"
+            },
+            [3] = {
+                strDescription = "Custom classes come with 2 extra slots for friends. You can add or remove friends using the CC Editor NPC at fountain. You can even make a friend an editor of your custom class. You cannot exceed this 2 slot limit.",
+                strImagePath = "UEb6P44"
+            },
+        }
+    },
+    [7] = {
+        strName = "Basing",
+        tblSlides = {
+            [1] = {
+                strTitle = "TitsRP Basing Tutorial",
+                strDescription = "It looks like you've just bought your first base! Here is a quick tutorial on how you might be raided, how to defend yourself in a raid, and how to use your base to it's fullest potential in order to make money, create items, grow drugs, etc.",
+                strImagePath = "k6x3ITr"
+            },
+            [2] = {
+                strTitle = "Securing your base",
+                strDescription = ("Players will try to raid your base to steal your money making entities such as your <ITEM:%s>, a <ITEM:%s>, or drugs such as <ITEM:%s>. You need more protection than a simple door can offer. This involves building walls between the raider and your money-making entities."):format(util.TableToJSON( { class = "sprinter_money_printer"}),util.TableToJSON( { class = "sugar_processor"}),util.TableToJSON( { class = "sugardrug_weed"})),
+                strImagePath = "7m17Ycw"
+            },
+            [3] = {
+                strTitle = "Designing your base",
+                strDescription = "The more walls your raider needs to break through, the more secure your base will be because it will be more difficult and expensive for them to raid you. It's up to you how to design your base. There are no restrictions on how complex it can be.",
+                strImagePath = "cPy7Wqp"
+            },
+            [4] = {
+                strTitle = "Building your base",
+                strDescription = "You may use any prop you like to build your base. VIPs can use the AdvDupe2 tool to save their base and spawn it back in at any time. This tool allows you to modify and improve your base design over time. For players with access to Advdupe, there are some really great player-made dupes available on the community forums at <URL:https://forums.titsrp.com>.",
+                strImagePath = "k6x3ITr"
+            },
+            [5] = {
+                strTitle = "Building your base (One-way Props)",
+                strDescription = "Make use of designs that include fences you can shoot through and one-way props that limit your raider's ability to see into your base while raiding. Click here to see a tutorial on how to make one-way props. <URL:https://steamcommunity.com/sharedfiles/filedetails/?id=2826519778>.",
+                strImagePath = "QmctR1e"
+            },
+            [6] = {
+                strTitle = "Building your base (Fading Doors & Keypads)",
+                strDescription = "You may use fading doors and keypads to create a path that your raider needs to follow to breach your base. Combining that with entities like motion detectors will give you an advantage by making their presence known and slowing them down while you counter the raid.",
+                strImagePath = "aTqAaJv"
+            },
+            [7] = {
+                strTitle = "Getting raided",
+                strDescription = "Players will try to raid your base to steal or destroy your money-making entities. Because players are limited by the number of their own money making entites that they can spawn, they will most often try to steal yours. Police may also try to raid you to destroy your entities if they are considered illegal under the law.",
+                strImagePath = "YFyDdfL"
+            },
+            [8] = {
+                strTitle = "Raiding tools & techniques (Explosives)",
+                strDescription = ("Players have a number of tools at their disposal to raid your base with. For more advanced base designs, a raider might use explosives such as a <ITEM:%s> or a weaker explosive with less range such as a <ITEM:%s> to blow straight through your walls. These and other types of explosives might temporarily destroy your walls or make them less effective by making them transparent."):format(util.TableToJSON( { class = "spawned_weapon", weaponclass = "weapon_c4"}), util.TableToJSON( { class = "spawned_weapon", weaponclass = "sugar_plantbomb"} )),
+                strImagePath = "vSXaCbW"
+            },
+            [9] = {
+                strTitle = "Raiding tools & techniques (Explosives)",
+                strDescription = "You need a large base with a ton of walls to defend against this type of raid since the explosive will only damage walls within a certain radius. More walls means the raider needs to deploy multiple explosives which will buy you time to heal and change defensive positions.",
+                strImagePath = "svofL7s"
+            },
+            [10] = {
+                strTitle = "Raiding tools & techniques (Hand Tools)",
+                strDescription = ("More hands-on raid methods include using tools such as a <ITEM:%s> or a <ITEM:%s>. These tools are much faster and more effective at breaching walls and doors respectively than explosives, but require the use of stealth to avoid detection and for the raider to be physically next to the wall. This makes them an easier target for defensive fire."):format(util.TableToJSON( { class = "spawned_weapon", weaponclass = "weapon_blowtorch"}), util.TableToJSON( { class = "spawned_weapon", weaponclass = "lockpick"} )),
+                strImagePath = "8s7GGVk"
+            },
+            [11] = {
+                strTitle = "Defending in a raid",
+                strDescription = ("Defending a raid can be difficult unless you have a large and intricate base design. Take advantage of items such as <ITEM:%s>, which can be crafted to increase your max HP or damage, a <ITEM:%s> to make yourself less visible to your raider, or an <ITEM:%s> to increase your armor. A combination of two or more of these items will give you the greatest advantage aginst your raider."):format(util.TableToJSON( { class = "sugar_food"}),util.TableToJSON( { class = "sugar_talisman"}),util.TableToJSON( { class = "armor_pack"})),
+                strImagePath = "5h154fX"
+            },
+            [12] = {
+                strTitle = "Stay prepared",
+                strDescription = ("Make sure you always have a <ITEM:%s> equipped to heal yourself, a <ITEM:%s>, and plenty of ammo because raids can last several minutes. A <ITEM:%s> can be crafted and used to provide a quick boost to your health and movement speed."):format(util.TableToJSON( { class = "spawned_weapon", weaponclass = "weapon_medkit"}),util.TableToJSON( { class = "armor_pack"}),util.TableToJSON( { class = "spawned_weapon", weaponclass = "rust_syringe"})),
+                strImagePath = "IDzMPAH"
+            },
+        }
+    },
+}
